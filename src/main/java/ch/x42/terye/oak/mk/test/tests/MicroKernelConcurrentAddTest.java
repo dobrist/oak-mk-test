0dfcfa0d3ae277788cd50809beea24ab321d4313
Damien Obrist <damien.obrist@gmail.com>
2013-02-07 10:27:27 +0100

    Small refactoring of the concurrent add test

--- src/main/java/ch/x42/terye/oak/mk/test/tests/MicroKernelConcurrentAddTest.java
+++ src/main/java/ch/x42/terye/oak/mk/test/tests/MicroKernelConcurrentAddTest.java
@@ -17,10 +17,10 @@ import ch.x42.terye.oak.mk.test.fixtures.MicroKernelTestFixture;
 
 /**
  * This test measures the performance of many threads concurrently adding a
- * large number of nodes. The test creates NB_THREADS threads, each of which
- * commits a subtree of height TREE_HEIGHT and branching factor
- * TREE_BRANCHING_FACTOR. The workers commit their nodes in batches of
- * COMMIT_RATE nodes per commit call.
+ * large number of nodes without conflicting each other. The test creates
+ * NB_THREADS threads, each of which commits a separate subtree of height
+ * TREE_HEIGHT and branching factor TREE_BRANCHING_FACTOR. The workers commit
+ * their nodes in batches of COMMIT_RATE nodes per commit call.
  */
 public class MicroKernelConcurrentAddTest extends MicroKernelPerformanceTest {
 
@@ -51,11 +51,13 @@ public class MicroKernelConcurrentAddTest extends MicroKernelPerformanceTest {
         workers = new LinkedList<Callable<String>>();
         for (int i = 0; i < NB_THREADS; i++) {
             MicroKernel mk = createMicroKernel();
-            Callable<String> worker = new SubtreeCommitter(mk, "node_" + i,
+            // commit the root nodes of the trees
+            String node = "node_" + i;
+            mk.commit("/", "+\"" + node + "\":{}", null, "");
+            // create worker
+            Callable<String> worker = new TreeCommitter(mk, "/" + node,
                     TREE_HEIGHT, TREE_BRANCHING_FACTOR, COMMIT_RATE);
             workers.add(worker);
-            // XXX: temporarily commit first node here to avoid any conflict
-            mk.commit("/", "+\"node_" + i + "\":{}", null, "");
         }
     }
 
@@ -82,88 +84,6 @@ public class MicroKernelConcurrentAddTest extends MicroKernelPerformanceTest {
     public void tearDownTest() {
         workers = null;
         System.gc();
-    }
-
-    /**
-     * This callable commits a subtree defined by the constructor arguments with
-     * a specified commit rate. The name of the nodes will be the concatenation
-     * of a constant prefix and a number corresponding to the zero-based
-     * numbering of the child nodes of a given node.
-     */
-    private class SubtreeCommitter implements Callable<String> {
-
-        private static final String NODE_NAME = "node_";
-
-        private MicroKernel mk;
-        private String root;
-        private int height;
-        private int branchingFactor;
-        private int rate;
-
-        public SubtreeCommitter(MicroKernel mk, String root, int height,
-                int branchingFactor, int rate) {
-            this.mk = mk;
-            this.root = root;
-            this.height = height;
-            this.branchingFactor = branchingFactor;
-            this.rate = rate;
-        }
-
-        @Override
-        public String call() throws Exception {
-            String r = null;
-            String batch = "";
-            int batchCount = 0;
-            // loop through all levels
-            // XXX: temporarily skip first level node
-            for (int i = 1; i <= height; i++) {
-                // number of nodes on this level
-                int nbNodes = (int) Math.pow(branchingFactor, i);
-                // loop through all nodes on this level
-                for (int j = 0; j < nbNodes; j++) {
-                    if (batchCount == rate) {
-                        // commit batch
-                        r = mk.commit("/", batch, null, "");
-                        batch = "";
-                        batchCount = 0;
-                    }
-                    // add new statement to batch for later commit
-                    batch += "+\"" + generatePath(i, j) + "\":{} ";
-                    batchCount++;
-                }
-            }
-            // commit remaining statements, if any
-            if (batchCount > 0) {
-                // commit batch
-                r = mk.commit("/", batch, null, "");
-            }
-            return r;
-        }
-
-        /**
-         * This method generates the path for a node in the subtree rooted at
-         * 'root'.
-         * 
-         * @param level the level of the node (0 being the same level as the
-         *            root of the subtree)
-         * @param index the zero-based index of the node on the specified level
-         * @return the absolute path of the specified node
-         */
-        private String generatePath(int level, int index) {
-            String path = root;
-            if (level == 0) {
-                return root;
-            }
-            // loop through all levels, starting at first sublevel of the root
-            for (int i = 1; i <= level; i++) {
-                // number of nodes at level 'level' that share the same ancestor
-                // node on the current level
-                int n = ((int) Math.pow(branchingFactor, level - i));
-                path += "/" + NODE_NAME + ((index / n) % branchingFactor);
-            }
-            return path;
-        }
-
     }
 
 }
---
+++ src/main/java/ch/x42/terye/oak/mk/test/tests/TreeCommitter.java
@@ -0,0 +1,99 @@
+package ch.x42.terye.oak.mk.test.tests;
+
+import java.util.concurrent.Callable;
+
+import org.apache.jackrabbit.mk.api.MicroKernel;
+import org.apache.jackrabbit.oak.commons.PathUtils;
+
+/**
+ * This callable commits a tree defined by the constructor arguments with a
+ * specified commit rate. The name of the nodes will be the concatenation of a
+ * constant prefix (NODE_PREFIX) and a number corresponding to the zero-based
+ * numbering of the child nodes of a given node.
+ */
+public class TreeCommitter implements Callable<String> {
+
+    private static final String NODE_PREFIX = "node_";
+
+    private MicroKernel mk;
+    private String root;
+    private int height;
+    private int branchingFactor;
+    private int rate;
+
+    /**
+     * Constructor.
+     * 
+     * @param mk the microkernel used to commit
+     * @param root absolute path to the already-existing root node of the tree
+     *            to be committed
+     * @param height the height of the tree to be committed
+     * @param branchingFactor the branching factor of the tree to be committed
+     * @param rate the commit rate (i.e number of nodes per commit)
+     */
+    public TreeCommitter(MicroKernel mk, String root, int height,
+            int branchingFactor, int rate) {
+        this.mk = mk;
+        this.root = root;
+        this.height = height;
+        this.branchingFactor = branchingFactor;
+        this.rate = rate;
+    }
+
+    @Override
+    public String call() throws Exception {
+        String revisionId = null;
+        String batch = "";
+        int batchCount = 0;
+        // loop through all levels
+        for (int i = 1; i <= height; i++) {
+            // number of nodes on this level
+            int nbNodes = (int) Math.pow(branchingFactor, i);
+            // loop through all nodes on this level
+            for (int j = 0; j < nbNodes; j++) {
+                if (batchCount == rate) {
+                    // commit batch
+                    revisionId = mk.commit("/", batch, null, "");
+                    batch = "";
+                    batchCount = 0;
+                }
+                // add new statement to batch for later commit
+                String abs = generatePath(i, j);
+                batch += "+\"" + PathUtils.relativize("/", abs) + "\":{} ";
+                batchCount++;
+            }
+        }
+        // commit remaining statements, if any
+        if (batchCount > 0) {
+            // commit batch
+            revisionId = mk.commit("/", batch, null, "");
+        }
+        // return last revision id
+        return revisionId;
+    }
+
+    /**
+     * This method generates the path for a node in the tree rooted at 'root'.
+     * 
+     * @param level the level of the node (0 being the same level as the root of
+     *            the tree)
+     * @param index the zero-based index of the node on the specified level
+     * @return the absolute path of the specified node
+     */
+    private String generatePath(int level, int index) {
+        String path = root;
+        if (level == 0) {
+            return root;
+        }
+        // loop through all levels, starting at first sublevel of the root
+        for (int i = 1; i <= level; i++) {
+            // number of nodes at level 'level' that share the same ancestor
+            // node on the current level
+            int n = ((int) Math.pow(branchingFactor, level - i));
+            path = PathUtils.concat(path, NODE_PREFIX
+                    + ((index / n) % branchingFactor));
+        }
+        return path;
+    }
+
+}
